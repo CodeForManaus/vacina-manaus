473cd05c5e5139755e89955e8a5f7e83af2c697f
Pedro Frota <pvfrota@gmail.com>
2021-01-26 18:35:35 -0400

    feat: New homepage

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    $.getJSON( "ajax/test.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+       
+        $( "<ul/>", {
+          "class": "my-new-list",
+          html: items.join( "" )
+        }).appendTo( "body" );
+      });
+
+});
