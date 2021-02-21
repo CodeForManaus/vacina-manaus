89336639c65eb059d0213083134e40784e48d7ea
Marcus Brandt <marcus.brandt@meliuz.com.br>
2021-02-20 23:31:02 -0400

    fix: Adiciona novamente os cards da home
    Esses cards foram retirados da home porque mostravam uma informação
    incorreta, agora eles estão buscando do csv correto e fazendo o parse da
    informação direto do nosso "back".
    
    Signed-off-by: Marcus Brandt <marcus.brandt@meliuz.com.br>

--- src/contexts/VaccineContext.js
+++ src/contexts/VaccineContext.js
@@ -10,20 +10,35 @@ const VaccineContextProvider = (props) => {
   const [vaccine, dispatch] = useReducer(vaccineReducer, [], () => {
     return {
       vaccinationStatistics: [],
-      vaccinationByDate: [],
+      vaccinationTrend: {},
+      vaccinationMovingAvg: {},
     }
   })
+
+  const lastObject = (obj) => {
+    return Object.keys(obj).length - 2
+  }
 
   const fetchData = useCallback(async () => {
     const vaccinationStatistics = await csv(
       `${process.env.REACT_APP_ANALYTICS_URL}/vaccination_count_statistics.csv`
     )
     delete vaccinationStatistics[0]['']
-    const vaccinationByDate = await csv(
-      `${process.env.REACT_APP_ANALYTICS_URL}/vaccine_date_count.csv`
+
+    const vaccinationFetch = await csv(
+      `${process.env.REACT_APP_ANALYTICS_URL}/vaccine_trend.csv`
     )
+    const vaccinationTrend = vaccinationFetch[lastObject(vaccinationFetch)]
+
+    const vaccinationMovingAvgFetch = await csv(
+      `${process.env.REACT_APP_ANALYTICS_URL}/vaccine_date_count_moving_avg.csv`
+    )
+    const vaccinationMovingAvg =
+      vaccinationMovingAvgFetch[lastObject(vaccinationMovingAvgFetch)]
+
     dispatch({ type: 'FETCH_VACCINATION_STATISTICS', vaccinationStatistics })
-    dispatch({ type: 'FETCH_VACCINATION_BY_DATE', vaccinationByDate })
+    dispatch({ type: 'FETCH_VACCINATION_TREND', vaccinationTrend })
+    dispatch({ type: 'FETCH_VACCINATION_MOVING_AVG', vaccinationMovingAvg })
   }, [])
 
   useEffect(() => {
@@ -31,7 +46,7 @@ const VaccineContextProvider = (props) => {
   }, [fetchData])
 
   return (
-    <VaccineContext.Provider value={{ vaccine: vaccine, dispatch }}>
+    <VaccineContext.Provider value={{ vaccine, dispatch }}>
       {props.children}
     </VaccineContext.Provider>
   )
